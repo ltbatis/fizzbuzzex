fe50d49792f6271397a22516c3d5116d283dec42
Lucas Batista <lucas.batista@f1rst.com.br>
2022-06-22 23:44:18 -0300

    add: fizzbuzz rule

--- lib/fizzbuzzex.ex
+++ lib/fizzbuzzex.ex
@@ -8,8 +8,19 @@ defmodule Fizzbuzzex do
   def handle_file_read({:ok, result}) do
     result
     |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&convert_and_evaluate_numbers/1)
   end
 
   def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
+  def convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_number()
+  end
+
+  def evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  def evaluate_number(number) when rem(number, 3) == 0, do: :fizz
+  def evaluate_number(number) when rem(number, 5) == 0, do: :buzz
+  def evaluate_number(number), do: number
 end
