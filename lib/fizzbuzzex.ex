3dbc7ebdf74f61fa3e35431e751bbd47ab51ad56
Lucas Batista <lucas.batista@f1rst.com.br>
2022-06-22 23:32:07 -0300

    add: error handling message

--- lib/fizzbuzzex.ex
+++ lib/fizzbuzzex.ex
@@ -11,5 +11,5 @@ defmodule Fizzbuzzex do
     |> Enum.map(&String.to_integer/1)
   end
 
-  def handle_file_read({:error, reason}), do: reason
+  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
 end
